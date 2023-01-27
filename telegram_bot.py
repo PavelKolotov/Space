a96f961b4af939ec48c30076e60c2d4fc43e06de
Pavel Kolotov <61968036+PavelKolotov@users.noreply.github.com>
2023-01-27 21:10:13 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,5 +5,4 @@ bot = telegram.Bot(token='5700058966:AAHE1BskR3-eEwvakuckuMocNILrV5P8Z1E')
 print(bot.get_me())
 
 
-
-bot.send_message(text='Hi John!', chat_id='@pavelsergeevich84')
+bot.send_document(chat_id='@pavelsergeevich84', document=open('images/nasa_epic_0.png', 'rb'))
